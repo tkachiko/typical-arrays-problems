5d9540384f860826a3b125a4d2806f866fb542a8
tkachiko <tkachikos@gmail.com>
2021-01-31 21:33:30 +0300

    feat: implement min, max & avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+exports.min = function min(array) {
+    return array && array.length ? Math.min(...array) : 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return array && array.length ? Math.max(...array) : 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let sum = 0;
+    if (array && array.length) {
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    } else return 0;
+};
